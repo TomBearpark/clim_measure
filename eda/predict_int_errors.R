1859ba9844528b1602cf18a6a6ad121d52a73370
TomBearpark <t.bearpark@googlemail.com>
2024-12-30 16:08:57 +0000

    clean up

--- eda/predict_int_errors.R
+++ eda/predict_int_errors.R
@@ -3,26 +3,31 @@ pacman::p_load(tidyverse, fixest, sf)
 if(Sys.info()['user'] == 'tombearpark'){
   dir <- '/Users/tombearpark/Library/CloudStorage/Dropbox/clim_measure/data/'
 } else {
-  dir <- "X"
+  dir <- "EDIT THIS PATH TO YOUR DATA FOLDER"
 }
 
-
 # load files --------------------------------------------------------------
+# Check file names
 list.files(dir) %>% str_subset("daymean")
 
 dist     <- read_csv(paste0(dir, "GHCNh_pairwisedistances_daymean_2011-2020_180plus.csv"))
 stations <- read_csv(paste0(dir, "GHCNh_stationlist_daymean_2011-2020_180plus.csv"))
 
-# Number of stations within 500, and minimum distance to another station
+# Some functions of distance that seem intuitive: 
+  # Number of stations within 500, 
+  # Minimum distance to another station, 
+  # Average distance to other stations that are within 500km
+
 dist.vals <- tibble(
   dist[,1], 
   dist_to_closest_stn = apply(dist[, -1], 1, function(x) min(x[x>0])), 
   num_within_500 = apply(dist[, -1], 1, function(x) sum(x < 500)), 
   dist_within_500 = apply(dist[, -1], 1, function(x) sum(x*(x < 500) / num_within_500))
 )
+stations <- stations %>% 
+  left_join(dist.vals)
 
-# start with a single year ------------------------------------------------
-
+# load data ---------------------------------------------------------------
 years <- 2011:2020
 
 df.vals <- map_dfr(years, function(year)
@@ -49,17 +54,32 @@ plt <- function(df.mod, var, scales = 'free'){
     facet_wrap(~name, scales=scales)
 }
 
+collapse <- function(df, group_vars, stations){
+  df %>% 
+    filter(!is.na(err)) %>% 
+    group_by(across(all_of(group_vars))) %>% 
+    summarize(mabs_err = mean(abs(err)),
+              mean_sqrd_err = mean(err^2),
+              clim = mean(t2m),
+              n = n()) %>% 
+    left_join(stations)  
+}
+
+
+covs <- c('Elevation', 'Latitude', 'Longitude', 'n', 
+          'dist_to_closest_stn', 'num_within_500', 'dist_within_500')
+covs.trans <- c("log(Elevation)", "log(abs(Latitude))", "log(abs(Longitude))", "log(n)", 
+                "log(dist_to_closest_stn)", "log(num_within_500)", "log(dist_within_500)")
+
+gen.ff <- function(covs){
+  paste0("log(mabs_err) ~ ", paste0(covs, collapse = " + ")) %>% 
+    as.formula()
+}
+
+
 # describe interpolation error by cross sectional covariates -------------------
 
-df.mod <- df %>% 
-  filter(!is.na(err)) %>% 
-  group_by(Station_ID) %>% 
-  summarize(mabs_err = mean(abs(err)),
-            mean_sqrd_err = mean(err^2),
-            clim = mean(t2m),
-            n = n()) %>% 
-  left_join(stations)  %>% 
-  left_join(dist.vals)
+df.mod <-collapse(df, "Station_ID", stations)
 
 plt(df.mod, "Elevation")
 plt(df.mod, "Latitude")
@@ -69,102 +89,26 @@ plt(df.mod, "dist_to_closest_stn")
 plt(df.mod, "num_within_500")
 plt(df.mod, "dist_within_500")
 
-feols(
-  log(mabs_err) ~ 
-    log(Elevation) + 
-    log(abs(Latitude)) + 
-    log(abs(Longitude)) +
-    log(n) + 
-    log(dist_to_closest_stn) + 
-    log(num_within_500) + 
-    log(dist_within_500)+
-    log(clim),
-  df.mod, 
-  vcov = 'hetero'
-) %>% 
+feols(gen.ff(covs.trans), df.mod, vcov = 'hetero') %>% 
   etable(fitstat = 'r2')
 
 # include year ------------------------------------------------------------
 
-df.mod <- df %>% 
-  filter(!is.na(err)) %>% 
-  group_by(Station_ID, year) %>% 
-  summarize(mabs_err = mean(abs(err)),
-            mean_sqrd_err = mean(err^2),
-            clim = mean(t2m),
-            n = n()) %>% 
-  left_join(stations)  %>% 
-  left_join(dist.vals)
+df.mod.y <- collapse(df, c("Station_ID", "year"), stations)
 
-feols(
-  log(mabs_err) ~ 
-    log(Elevation) + 
-    log(abs(Latitude)) + 
-    log(abs(Longitude)) +
-    log(n) + 
-    log(dist_to_closest_stn) + 
-    log(num_within_500) + 
-    log(clim), 
-  df.mod, 
-  vcov = 'hetero', 
-  split = ~year
-) %>% 
+feols(gen.ff(covs.trans), df.mod.y, vcov = 'hetero', split= ~year) %>% 
   coefplot()
 
-feols(
-  log(mabs_err) ~ 
-    log(Elevation) + 
-    log(abs(Latitude)) + 
-    log(abs(Longitude)) +
-    log(n) + 
-    log(dist_to_closest_stn) + 
-    log(num_within_500) + 
-    log(clim) + 
-    i(year), 
-  df.mod, 
-  vcov = 'hetero', 
-) %>% 
-  coefplot(drop = 'Const')
+feols(gen.ff(c(covs.trans, "i(year)")), df.mod.y, vcov = 'hetero') %>% 
+  coefplot()
 
 # by month of year --------------------------------------------------------
 
-df.mod <- df %>% 
-  filter(!is.na(err)) %>% 
-  mutate(month = lubridate::month(date)) %>%
-  group_by(Station_ID, month) %>% 
-  summarize(mabs_err = mean(abs(err)),
-            mean_sqrd_err = mean(err^2),
-            clim = mean(t2m),
-            n = n()) %>% 
-  left_join(stations)  %>% 
-  left_join(dist.vals)
+df.mod.m <- collapse(df %>% 
+                       mutate(month = month(date)), c("Station_ID", "month"), stations)
 
-feols(
-  log(mabs_err) ~ 
-    log(Elevation) + 
-    log(abs(Latitude)) + 
-    log(abs(Longitude)) +
-    log(n) + 
-    log(dist_to_closest_stn) + 
-    log(num_within_500) + 
-    log(clim), 
-  df.mod, 
-  vcov = 'hetero', 
-  split = ~month
-) %>% 
+feols(gen.ff(covs.trans), df.mod.m, vcov = 'hetero', split= ~month) %>% 
   coefplot()
 
-feols(
-  log(mabs_err) ~ 
-    log(Elevation) + 
-    log(abs(Latitude)) + 
-    log(abs(Longitude)) +
-    log(n) + 
-    log(dist_to_closest_stn) + 
-    log(num_within_500) + 
-    log(clim) + 
-    i(month), 
-  df.mod, 
-  vcov = 'hetero', 
-) %>% 
-  coefplot(drop = 'Const')
+feols(gen.ff(c(covs.trans, "i(month)")), df.mod.m, vcov = 'hetero') %>% 
+  etable()
